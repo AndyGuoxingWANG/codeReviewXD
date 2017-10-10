a895ff273736d169b2e16407c22f463908d0acab
OneOneBee <congniu1263@gmail.com>
2017-10-10 09:13:39 -0700

    Add files via upload

--- journal.c
+++ journal.c
@@ -4,12 +4,11 @@
  *  Definition of a Journal
  *
  *  Created on: Oct 31, 2017
- *  Author: esandar
+ *  Author: philip gust
  */
 
 #include "journal.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 /**  Size of allocated journals pointer array */
@@ -18,17 +17,6 @@ static size_t JOURNALS_SIZE = 0;
 /** pointer to allocated journals pointer array */
 static Journal **journals = NULL;
 
-/**
- * Initialize journal cache. Call from perations that access it
- */
-static void initJournalsCache(void) {
-	if (journals == NULL) {
-		JOURNALS_SIZE = 4; //size of journal array
-		journals = malloc(JOURNALS_SIZE * sizeof (Journal *));
-		journals[0] = NULL; // null terminate array
-	}
-
-}
 /**
   * Print a Journal
   * @param journal the journal to print
@@ -42,6 +30,17 @@ void printJournal(const Journal *journal) {
 }
 
 /**
+ * Initialize journals cache. Call from operations that access it
+ */
+static void initJournalsCache(void) {
+	if (journals == NULL) {
+		JOURNALS_SIZE = 4; // size of journals array
+		journals = malloc(JOURNALS_SIZE * sizeof(Journal*));
+		journals[0] = NULL; // null terminate array
+	}
+}
+
+/**
   * Create a journal from its information
   * @param issn the journal Issn
   * @param name the journal name
@@ -50,31 +49,30 @@ void printJournal(const Journal *journal) {
   * @return pointer to journal struct, or to saved journal if journal is NULL
   * 	   (NULL if not defined)
   */
-Journal* createJournal(Issn issn, const char* name, Publisher *pub, Journal *journal) {
+Journal* createJournal(Issn issn, const char* name, Publisher* pub, Journal* journal) {
 	if (journal == NULL) {
 		initJournalsCache();
 
-		//find journal in array
-		int pindex = 0;
-		for ( ; journals[pindex] != NULL; pindex++) { //while not as sentinel entry
-			if (journal->issn == issn) {
-				return journals[pindex];	// return matching journal
+		// find journal in array
+		int jindex = 0;
+		for ( ; journals[jindex] != NULL; jindex++) { // while not at sentinel entry
+			if (journals[jindex]->issn == issn) {
+				return journals[jindex]; // return matching journal
 			}
 		}
 
-		//if journal not found, allocate new struct and add to array
-		if (pindex + 1 >= JOURNALS_SIZE) {
-			JOURNALS_SIZE *= 2; //double size of journals array
-			journals = realloc (journals, JOURNALS_SIZE * sizeof (Journal*));
-
+		// if journal not found, allocate new struct and add to array
+		if (jindex + 1 >= JOURNALS_SIZE) {
+			JOURNALS_SIZE *= 2; // double size of journals array
+			journals = realloc(journals, JOURNALS_SIZE * sizeof(Journal*));
 		}
-		journal = malloc ( sizeof(Journal)); //allocate storage for new publisher
-		journals[pindex] = journal;	//add pointer to new Publisher to array;
-		journals[++pindex] = NULL;	//add null pointer after new one
+		journal = malloc(sizeof(Journal)); // allocate storage for new journal
+		journals[jindex] = journal; // add pointer to new Journal to array
+		journals[++jindex] = NULL; // add null pointer after new one
 	}
-	journal->issn = issn;
 	strcpy(journal->name, name);
-	strcpy(journal->publisher->name,pub->name);
+	journal->issn = issn;
+	journal->publisher = pub;
 	return journal;
 }
 
@@ -85,12 +83,14 @@ Journal* createJournal(Issn issn, const char* name, Publisher *pub, Journal *jou
   */
 Journal* getJournalByIssn(Issn issn) {
 	initJournalsCache();
+
 	//find journal in array
 	int pindex = 0;
-	for ( ; journals[pindex] != NULL; pindex++) {	//while not at sentinel entry
+	for ( ; journals[pindex] != NULL; pindex++) { // while not at sentinel entry
 		if (journals[pindex]->issn == issn) {
-			return journals[pindex];	//return matching journal
+			return journals[pindex]; // return matching journal
 		}
 	}
+	// if not found in journal array, return NULL
 	return NULL;
 }
